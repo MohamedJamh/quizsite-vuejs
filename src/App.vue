436fa7237b5fb168ce894a4e65ab4672534690ba
MohamedJamh <jam7.mohamed@gamil.com>
2023-03-01 17:31:06 +0100

    rearranging files

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,35 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+    <p>{{pages[0].content}}</p>
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
+<script>
+import PageViewer from './components/PageViewer.vue'
+
+export default{
+    components:{
+        PageViewer
+    },
+    data(){
+        return{
+            activePage: 0,
+            pages:[
+                {
+                    link : {text:'Home',url:'index.html'},
+                    pageTitle:'lets Learn vuejs',
+                    content : 'Vuejs is easy if you already now basic html javascript. So go for it'
+                },
+                {
+                    link : {text:'About',url:'About.html'},
+                    pageTitle:'About us',
+                    content : 'We are students from youcode'
+                },
+                {
+                    link : {text:'contact',url:'contact.html'},
+                    pageTitle:'Contact us',
+                    content : 'Visit our web site'
+                },
+            ]
+        };
+    }
 }
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
+</script>
---
+++ src/components/PageViewer.vue
@@ -0,0 +1,12 @@
+<template>
+    <div>
+        <h1 class="text-3xl">{{page.pageTitle}}</h1>
+        <p>{{page.content}}</p>
+    </div>
+</template>
+
+<script>
+export default{
+    props : ['page']
+}
+</script>
