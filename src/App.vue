2f045347c32f41d331f680a946f7c818912bdcaf
MohamedJamh <jam7.mohamed@gmail.com>
2023-03-11 19:51:46 +0100

    working with components , methods , localstorage , empty props

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,49 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <navbar
+        :theme="theme"
+        :activePage="activePage"
+        :pages="pages"
+        :nav-link-click="(index) => activePage = index"
+    ></navbar>
+
+    <page-viewer
+        v-if="pages.length > 0"
+        :page="pages[activePage]"
+    ></page-viewer>
 </template>
-
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Navbar from './components/Navbar.vue';
+import PageViewer from './components/PageViewer.vue';
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+export default{
+    components:{
+        Navbar,
+        PageViewer
+    },
+    created(){
+        this.getPages();
+        this.getThemeSettings()
+    },
+    data(){
+        return{
+            theme: '-',
+            activePage: 0,
+            pages: []
+        };
+    },
+    methods:{
+        async getPages(){
+            let res = await fetch('page.json')
+            let data = await res.json();
+
+            this.pages = data
+        },
+        getThemeSettings(){
+            let theme = localStorage.getItem('theme')
+            if(theme){
+                this.theme = theme
+            }
+        }
+    }
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/components/NavbarLink.vue
@@ -0,0 +1,29 @@
+<template>
+    <a 
+        class="text-sm"
+        :class="activeClasses"
+        :title="`This title goes to ${page.link.url}`"
+        :href="page.link.url"
+    >{{page.link.text}}</a>
+
+</template>
+
+<script>
+export default{
+    props : ['page','isActive'],
+    computed:{
+        activeClasses(){
+            return{
+                'text-white': this.isActive,
+                emphasize : this.isActive 
+            };
+        }
+    }
+}
+</script>
+
+<style>
+    .emphasize{
+        text-decoration: underline;
+    }
+</style>
