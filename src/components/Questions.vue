b2649cf4e1eb4b96ec7a897df607e4a91d60a28c
MohamedJamh <jam7.mohamed@gmail.com>
2023-03-13 14:13:25 +0100

    working on questions

--- src/components/Questions.vue
+++ src/components/Questions.vue
@@ -1,11 +1,18 @@
 <template>
     <div class="container">
+        {{ score }}
         <div class="bg-white w-full rounded-lg p-5">
             <div class="my-2">
                 <p class="font-semibold text-3xl">{{ questions[currentQuestion].question }}</p>
             </div>
             <div class="my-2">
-                <div v-for="(choice , index) in questions[currentQuestion].choices" :key="index" class="border rounded-lg border-slate-500 p-3 my-3">
+                <div 
+                v-for="(choice , index) in questions[currentQuestion].choices" 
+                :key="index"
+                :ref="`answer-${index}`"
+                :class="{'bg-sky-900 text-white font-bold': this.selctedAnswer == index}"
+                class="border rounded-lg border-slate-500 p-3 my-3 cursor-pointer"
+                @click="submitQuestion(index)">
                     <p>
                         {{ choice }}
                     </p>
@@ -13,12 +20,17 @@
             </div>
             <div class="flex gap-3 items-center">
                 <div class="border w-full rounded-xl overflow-hidden">
-                    <div class="bg-sky-100 text-center">0%</div>
+                    <div 
+                    style="transition-duration: 0.3s;" 
+                    :style="`width:${percentage}%`" 
+                    class="bg-sky-900 text-white font-bold text-center w-1/2"
+                    >{{ percentage }}%</div>
                 </div>
-                <button 
+                <button
+                    v-if="currentQuestion == this.questions.length - 1"
                     class="bg-rose-600 px-10 py-2 rounded-lg text-white text-xl"
-                    @click="nextQuestion()"
-                >Next</button>
+                    @click="this.$parent.activeStep = 2"
+                >Finish</button>
             </div>
         </div>
     </div>
@@ -27,18 +39,41 @@
 <script>
 export default {
     props : ['questions'],
+    created(){
+        this.percentage = this.perQuestion
+        this.questions.sort(function(){ return Math.random() - 0.5; })
+    },
     data(){
         return{
-            currentQuestion : 0
+            selctedAnswer : null,
+            currentQuestion : 0,
+            perQuestion : 100 / this.questions.length,
+            percentage : 0,
+            score:0,
+            log : []
         };
     },
     methods: {
+        submitQuestion(index){
+            if(this.currentQuestion + 1 != this.questions.length){
+
+                this.selctedAnswer = index
+                if(index+1 == this.questions[this.currentQuestion].answer){
+                    this.score += this.perQuestion
+                }
+                setTimeout(function(){
+                    this.nextQuestion()
+                }.bind(this),1000);
+            }
+        },
         nextQuestion(){
-            this.currentQuestion += 1
+            this.currentQuestion += 1 
+            this.selctedAnswer = null
+            // moving to next question
+            this.percentage += this.perQuestion
         }
     },
 }
 </script>
 <style>
-    
 </style>
